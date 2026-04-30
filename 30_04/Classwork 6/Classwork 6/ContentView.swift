98d24d3071edac46a14b65714be23bc832e00637
bonjourclaudio <claudio.weckherlin@gmail.com>
2026-04-30 14:31:03 +0200

    implement widget struct

--- 30_04/Classwork 6/Classwork 6/ContentView.swift
+++ 30_04/Classwork 6/Classwork 6/ContentView.swift
@@ -9,80 +9,34 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            VStack(alignment: .center, spacing: 10) {
-                VStack(alignment: .leading, spacing: 10) {
-                    VStack(alignment: .leading) {
-                      // Space Between
-                        Text("Output 22")
-                          .font(
-                            Font.custom("SF Pro Display", size: 14)
-                                .weight(.bold)
-                          )
-                          .kerning(0.154)
-                          .foregroundColor(.white)
-                          .frame(maxWidth: .infinity, alignment: .bottomLeading)
-                      Spacer()
-                      // Alternating Views and Spacers
-                        VStack(alignment: .leading, spacing: 0) {
-                            Text("Semester End Night")
-                              .font(
-                                Font.custom("SF Pro Display", size: 28)
-                                  .weight(.bold)
-                              )
-                              .kerning(0.396)
-                              .foregroundColor(.white)
-                              .frame(maxWidth: .infinity, alignment: .bottomLeading)
-                            
-                            Text("Saturday 18 June • 22:00 • 7 CHF* ")
-                              .font(Font.custom("SF Pro Display", size: 16))
-                              .foregroundColor(.white)
-                              .frame(maxWidth: .infinity, alignment: .bottomLeading)
-                        }
-                        .padding(0)
-                        .frame(maxWidth: .infinity, alignment: .topLeading)
-                    }
-                    .padding(0)
-                    .frame(maxWidth: .infinity, minHeight: 132, maxHeight: 132, alignment: .leading)
-                }
-                .padding(.horizontal, 18)
-                .padding(.vertical, 16)
-                .frame(maxWidth: .infinity, alignment: .topLeading)
-                .background(
-                  LinearGradient(
-                    stops: [
-                      Gradient.Stop(color: Color("MehrspurEventColor").opacity(0), location: 0.00),
-                      Gradient.Stop(color: Color("MehrspurEventColor"), location: 1.00),
-                    ],
-                    startPoint: UnitPoint(x: 0.01, y: 0),
-                    endPoint: UnitPoint(x: 0.01, y: 0.97)
-                  )
-                )
-                .background(
-                  Image("mehrspur-poster")
-                    .resizable()
-                    .aspectRatio(contentMode: .fill)
-                    .frame(width: 350, height: 164)
-                    .clipped()
-                )
-                .cornerRadius(30)
-                
-                
-                // 2ND ROW - MEHRSPUR
-                Text("Mehrspur")
-                  .font(
-                    Font.custom("SF Pro Display", size: 14)
-                      .weight(.semibold)
-                  )
-                  .multilineTextAlignment(.center)
-                  .foregroundColor(.black)
-                  .frame(maxWidth: .infinity, alignment: .top)
-                
-            }
-            .padding(0)
-            .frame(width: 350, height: 248, alignment: .top)
-        }
-        .padding()
+    
+        let crazyParty = WidgetEvent(
+            title: "CrazyParty",
+            subtitle: "craaaazy",
+            widgetLabel: "Crazy",
+            backgroundFileTitle: "mehrspur-poster",
+            date: "2026-05-01",
+            time: "18:00",
+            price: 19.99,
+            color: Color("MehrspurEventColor")
+        )
+        
+        WidgetView(widgetEvent: crazyParty)
+        
+        
+        let cooleParty = WidgetEvent(
+            title: "so cool",
+            subtitle: "mega cool",
+            widgetLabel: "cooool",
+            backgroundFileTitle: "mehrspur-poster",
+            date: "2030-05-01",
+            time: "02:00",
+            price: 999.99,
+            color: Color(.green)
+        )
+        
+        WidgetView(widgetEvent: cooleParty)
+        
     }
 }
 
--- 30_04/Classwork 6/Classwork 6/Widget.swift
+++ 30_04/Classwork 6/Classwork 6/Widget.swift
@@ -26,3 +26,99 @@ struct WidgetEvent : Identifiable {
     let color : Color
     let preferredSize : WidgetSize = . medium
 }
+
+let staticWidgetEvent: WidgetEvent = WidgetEvent(
+    title: "Semester End Party",
+    subtitle: "Output 22",
+    widgetLabel: "Mehrspur",
+    backgroundFileTitle: "mehrspur-poster",
+    date: "Saturday 18 June",
+    time: "22:00",
+    price: 7.0,
+    color: .blue
+)
+
+struct WidgetView: View {
+    let widgetEvent: WidgetEvent
+    
+    var body: some View {
+        VStack {
+            VStack(alignment: .center, spacing: 10) {
+                VStack(alignment: .leading, spacing: 10) {
+                    VStack(alignment: .leading) {
+                      // Space Between
+                        Text(widgetEvent.title)
+                          .font(
+                            Font.custom("SF Pro Display", size: 14)
+                                .weight(.bold)
+                          )
+                          .kerning(0.154)
+                          .foregroundColor(.white)
+                          .frame(maxWidth: .infinity, alignment: .bottomLeading)
+                      Spacer()
+                      // Alternating Views and Spacers
+                        VStack(alignment: .leading, spacing: 0) {
+                            Text(widgetEvent.subtitle)
+                              .font(
+                                Font.custom("SF Pro Display", size: 28)
+                                  .weight(.bold)
+                              )
+                              .kerning(0.396)
+                              .foregroundColor(.white)
+                              .frame(maxWidth: .infinity, alignment: .bottomLeading)
+                            
+                            Text("\(widgetEvent.date) • \(widgetEvent.time) • \(widgetEvent.price, specifier: "%.2f") CHF* ")
+                              .font(Font.custom("SF Pro Display", size: 16))
+                              .foregroundColor(.white)
+                              .frame(maxWidth: .infinity, alignment: .bottomLeading)
+                        }
+                        .padding(0)
+                        .frame(maxWidth: .infinity, alignment: .topLeading)
+                    }
+                    .padding(0)
+                    .frame(maxWidth: .infinity, minHeight: 132, maxHeight: 132, alignment: .leading)
+                }
+                .padding(.horizontal, 18)
+                .padding(.vertical, 16)
+                .frame(maxWidth: .infinity, alignment: .topLeading)
+                .background(
+                  LinearGradient(
+                    stops: [
+                        Gradient.Stop(color: widgetEvent.color.opacity(0), location: 0.00),
+                      Gradient.Stop(color: widgetEvent.color, location: 1.00),
+                    ],
+                    startPoint: UnitPoint(x: 0.01, y: 0),
+                    endPoint: UnitPoint(x: 0.01, y: 0.97)
+                  )
+                )
+                .background(
+                    Image(widgetEvent.backgroundFileTitle)
+                    .resizable()
+                    .aspectRatio(contentMode: .fill)
+                    .frame(width: 350, height: 164)
+                    .clipped()
+                )
+                .cornerRadius(30)
+                
+                
+                // 2ND ROW - MEHRSPUR
+                Text("Mehrspur")
+                  .font(
+                    Font.custom("SF Pro Display", size: 14)
+                      .weight(.semibold)
+                  )
+                  .multilineTextAlignment(.center)
+                  .foregroundColor(.black)
+                  .frame(maxWidth: .infinity, alignment: .top)
+                
+            }
+            .padding(0)
+            .frame(width: 350, height: 248, alignment: .top)
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    WidgetView(widgetEvent: staticWidgetEvent)
+}
